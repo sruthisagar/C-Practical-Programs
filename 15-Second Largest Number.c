2518c5cd19b1f100c31247197ff236288fb73eb2
sruthisagar <sruthidingu@gmail.com>
2020-09-14 14:17:32 +0530

    Second Largest Edit

--- 15-Second Largest Number.c
+++ 15-Second Largest Number.c
@@ -1,28 +1,31 @@
 //2nd largest no
 
 #include<stdio.h>
+#include <limits.h>
 void main()
 {
 	int num,n,i,l1,l2;
+	l1=l2=INT_MIN;
 	printf("Enter the number of numbers\n");
 	scanf("%i",&n);
 	printf("Enter the numbers\n");
 	for(i=0;i<n;i++)
 	{
 		scanf("%i",&num);
-		if(i==0)
-			l1=l2=num;
-		else
+		if (num>l2)
 		{
-			if(num>l1)
+			if (num>l1)
 			{
-				l2=l1;
-				l1=num;
+				l2 = l1;
+				l1 = num;
 			}
-			else
-				if(num>l2)
-					l2=num;		
+			else if(num!=l1)
+				l2 = num;
 		}
 	}
-	printf("The second largest number is %i\n",l2);
+
+	if(l2==INT_MIN)
+		printf("Second largest number is not present\n");
+	else
+		printf("The second largest number is %i\n",l2);
 }
